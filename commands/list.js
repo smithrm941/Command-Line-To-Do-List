f5821f738d5f3e5672b0f9dffde8000012a386ea
smithrm941 <smithrm941@gmail.com>
2017-07-19 16:18:24 -0700

    added list as a sub-command to tasks.js

--- commands/list.js
+++ commands/list.js
@@ -2,12 +2,16 @@ const fs = require('fs')
 const path = require('path')
 const TASKFILEPATH = path.resolve(__dirname, '../tasks.json')
 
-fs.readFile(TASKFILEPATH, 'utf8', (err, taskFileContents) => {
-  const parsedObject = JSON.parse( taskFileContents )
-  const taskArray = parsedObject.taskStorage
-  console.log("ID Description")
-  console.log("-- -----------")
-  for (var i = 0; i < taskArray.length; i++) {
-    console.log(`${taskArray[i].id}  ${taskArray[i].name}`)
-  }
-})
+function list() {
+  fs.readFile(TASKFILEPATH, 'utf8', (err, taskFileContents) => {
+    const parsedObject = JSON.parse( taskFileContents )
+    const taskArray = parsedObject.taskStorage
+    console.log("ID Description")
+    console.log("-- -----------")
+    for (var i = 0; i < taskArray.length; i++) {
+      console.log(`${taskArray[i].id}  ${taskArray[i].name}`)
+    }
+  })
+}
+
+module.exports = list
--- tasks.js
+++ tasks.js
@@ -1,11 +1,15 @@
 #! /usr/local/bin/node
  const add = require('./commands/add')
+ const list = require('./commands/list')
 
 const method = process.argv[2]
 
 switch (method) {
   case 'add':
     add(process.argv[3])
+    break;
+  case 'list':
+    list()
     break;
   default:
 }
