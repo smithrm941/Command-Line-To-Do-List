9037c3a20f5dc316abd4259736bd12661dac687c
smithrm941 <smithrm941@gmail.com>
2017-07-19 16:08:08 -0700

    added list function

--- commands/list.js
+++ commands/list.js
@@ -1,1 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+const TASKFILEPATH = path.resolve(__dirname, '../tasks.json')
 
+fs.readFile(TASKFILEPATH, 'utf8', (err, taskFileContents) => {
+  const parsedObject = JSON.parse( taskFileContents )
+  const taskArray = parsedObject.taskStorage
+  console.log("ID Description")
+  console.log("-- -----------")
+  for (var i = 0; i < taskArray.length; i++) {
+    console.log(`${taskArray[i].id}  ${taskArray[i].name}`)
+  }
+})
