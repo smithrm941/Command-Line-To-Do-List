538ce785f2c701610e2558476afcd0fdd94060bc
smithrm941 <smithrm941@gmail.com>
2017-07-24 12:58:32 -0700

    added mocha and chai and file where tests would go

---
+++ tasks-test.js
@@ -0,0 +1,5 @@
+var expect = require('chai').expect;
+var add = require('./tasks.js').add;
+var list = require('./tasks.js').list;
+var deleteTask = require('./tasks.js').deleteTask;
+var complete = require('./tasks.js').complete;
